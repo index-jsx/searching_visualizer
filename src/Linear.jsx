b72adab9a6cb2d41b9edf739513a8cad6fda064a
index-jsx <mervinmervin0660@gmail.com>
2025-10-10 21:11:51 +0530

    Fix import path for LinearCode component

--- src/Linear.jsx
+++ src/Linear.jsx
@@ -3,7 +3,8 @@ import "./Linear.css";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import Complexity from "./Complexity";
-import LinearCode from "./linearCode";
+import LinearCode from "./linearCode.jsx";
+
 export default function Linear() {
     const [array, setArray] = useState(Genarate_linear_array());
     const [arrayctn, setArrayctn] = useState();
